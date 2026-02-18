b68e38604fac40a7b3a11dbb1d62f5a87b91fabf
Snitrxm <andre.oliveira.roch@hotmail.com>
2026-02-18 16:26:33 +0000

    Add next-themes, fix turbopack root

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
     poweredByHeader: false,
-    turbopack: {},
+    turbopack: {
+        root: __dirname,
+    },
     images: {
         formats: ["image/avif", "image/webp"],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920],
